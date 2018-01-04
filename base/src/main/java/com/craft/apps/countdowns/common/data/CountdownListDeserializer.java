eae1196cc28d1826348cbd16ec8184abe6170e05
Willie Chalmers III <willieechalmers@gmail.com>
2018-01-03 23:00:34 -0600

    Fix CountdownListDeserializer  - Fix silly reference to RTDB

--- base/src/main/java/com/craft/apps/countdowns/common/data/CountdownListDeserializer.java
+++ base/src/main/java/com/craft/apps/countdowns/common/data/CountdownListDeserializer.java
@@ -4,28 +4,27 @@ import android.arch.core.util.Function;
 import android.util.Log;
 
 import com.craft.apps.countdowns.common.model.Countdown;
-import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * A function that maps a {@link DataSnapshot} into a list of {@link Countdown}s.
+ * A function that maps a {@link com.google.firebase.firestore.DocumentReference} into a list
+ * of {@link Countdown}s.
  *
- * @version 1.0.0
+ * @version 2.0.0
  * @since 2.0.0
  */
-public class CountdownListDeserializer implements Function<DataSnapshot, List<Countdown>> {
+public class CountdownListDeserializer implements Function<QuerySnapshot, List<Countdown>> {
 
     private static final String TAG = CountdownListDeserializer.class.getSimpleName();
 
     @Override
-    public List<Countdown> apply(DataSnapshot input) {
+    public List<Countdown> apply(QuerySnapshot input) {
         List<Countdown> countdowns = new ArrayList<>();
         try {
-            for (DataSnapshot dataSnapshot : input.getChildren()) {
-                countdowns.add(dataSnapshot.getValue(Countdown.class));
-            }
+            countdowns = input.toObjects(Countdown.class);
         } catch (Exception e) {
             // Recoverable
             Log.w(TAG, "Error when fetching countdowns", e);
