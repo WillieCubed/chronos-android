43eb905a1186cd6c2d3007e80c95b47d92c2fb51
Willie Chalmers III <willie@williecubed.me>
2024-01-07 13:30:55 -0800

    Rewrite test templates using Kotlin and JUnit4

--- app/src/androidTest/java/com/craft/apps/countdowns/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/craft/apps/countdowns/ExampleInstrumentedTest.kt
@@ -1,28 +1,24 @@
-package com.craft.apps.countdowns;
+package com.craft.apps.countdowns
 
-import static org.junit.Assert.assertEquals;
-
-import android.content.Context;
-
-import androidx.test.core.app.ApplicationProvider;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
 
 /**
  * Instrumentation test, which will execute on an Android device.
  *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * @see [Testing documentation](http://d.android.com/tools/testing)
  */
-@RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
-
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() throws Exception {
+    @Throws(Exception::class)
+    fun useAppContext() {
         // Context of the app under test.
-        Context appContext = ApplicationProvider.getApplicationContext();
-
-        assertEquals("com.craft.apps.countdown", appContext.getPackageName());
+        val appContext = ApplicationProvider.getApplicationContext<Context>()
+        Assert.assertEquals("com.craft.apps.countdowns", appContext.packageName)
     }
 }
